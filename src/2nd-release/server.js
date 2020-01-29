0943c91695415960a72a002d2ddad6ab70b2bd35
Matheus Cassiano <matheuscassiano9@gmail.com>
2020-01-29 17:29:36 +0000

    Styling page

---
+++ src/2nd-release/server.js
@@ -0,0 +1,45 @@
+import express from 'express'
+import http from 'http'
+import createGame from './public/game.js'
+import socketio from 'socket.io'
+
+const app = express()
+const server = http.createServer(app)
+const sockets = socketio(server)
+
+app.use(express.static('public'))
+
+const game = createGame()
+game.start()
+
+game.subscribe((command) => {
+    console.log(`> Emitting ${command.type}`)
+    sockets.emit(command.type, command)
+})
+
+console.log(game.state)
+
+sockets.on('connection', (socket) => {
+    const playerId = socket.id
+    console.log(`> Player connected: ${playerId}`)
+
+    game.addPlayer({playerId: playerId });
+
+    socket.emit('setup', game.state)
+ 
+    socket.on('disconnect', () => {
+        game.removePlayer({ playerId: playerId })
+        console.log(`> Player disconnected: ${playerId}`)
+    })
+
+    socket.on('move-player', (command) => {
+        command.playerId = playerId
+        command.type = 'move-player'
+
+        game.movePlayer(command)
+    })
+})
+
+server.listen(3000, () => {
+    console.log(`> Sever listening on port: 3000`)
+})
